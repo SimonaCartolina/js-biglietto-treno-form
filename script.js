695de295383e7752c49022bf423366976adcecc2
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-15 18:17:10 +0200

    form of train tickets

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const kilometri;
+const UserAge;
 let costoalkm= parseInt(0.233.toFixed(2) + €);
 let costocomplessivo= costoalkm * kilometri;
 let costoscontato;
@@ -13,7 +14,6 @@ if (userAge < 18) {
 
 
 document.getElementById('kilometri') .innerHTML = kilometri;
-document.getElementById('costototale') .innerHTML = costoscontato;
 
 console.log(kilometri);
 console.log(userAge);
@@ -21,17 +21,23 @@ console.log(costoalkm);
 console.log(costocomplessivo);
 console.log(costoscontato);
 
-let Submit= document.getElementById('Submit');
+if(userAge< 18){
+    costoscontato= ((19.4 * costocomplessivo) / 100).toFixed(2);
+} else if (userAge > 65) {
+    costoscontato= ((37.7 * costocomplessivo) / 100).toFixed(2);
+} else if{
+    costoscontato=costocomplessivo.toFixed(2);
+} 
 
-Submit.addEventListener ('click' , function()
-{
-    if(click){
-        costoscontato= ((19.4 * costocomplessivo) / 100).toFixed(2);
-    } else if (userAge > 65) {
-        costoscontato= ((37.7 * costocomplessivo) / 100).toFixed(2);
-    } else if{
-        costoscontato=costocomplessivo.toFixed(2);
-    } 
+document.getElementById('costototale').innerHTML=+ costoscontato;
+
+
+let Submit;
+Submit= document.querySelector('#Submit');
+console.log('Submit');
+
+Submit.addEventListener ('click', function(){
+
     if (userAge> 120){
         alert('Invalid digit');
     }
